711957defa867b643fbba8776c170269347c3df7
Sergio <sergioa.orozcoo@gmail.com>
2024-02-23 22:37:21 -0500

    Adding Play Icon

---
+++ src/assets/PlayIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+const props = defineProps(['isPlay'])
+</script>
+<template>
+  <svg v-if="!isPlay" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play-filled"
+    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+      stroke-width="0" fill="currentColor" />
+  </svg>
+  <svg v-else xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-pause-filled"
+    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+      stroke-width="0" fill="currentColor" />
+    <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+      stroke-width="0" fill="currentColor" />
+  </svg>
+</template>
---
+++ src/islands/SpotiyPlayer.vue
@@ -0,0 +1,43 @@
+<script lang="js" setup>
+  import {ref} from 'vue'
+  import PlayIcon from "../assets/PlayIcon.vue";
+  const itemPlaying = ref(null)
+  const props = defineProps(['songs'])
+  const playSong = (item, index) => {
+    const player =  document.getElementById(`spotiSong_${index}`);
+    if(player.paused) {
+      player.play()
+      itemPlaying.value = ref(index)
+    } else {
+      player.pause()
+      itemPlaying.value = ref(null)
+    }
+  }
+
+  const checkProp = (index) => {
+    return itemPlaying.value === 1
+  }
+
+</script>
+<template>
+  <div class="flex flex-col gap-y-3 px-3 py-3 bg-slate-800/40 border border-slate-600 rounded-md">
+    <div class="flex justify-between items-center mb-4">
+      <p class="text-slate-400 font-semibold text-xl">Trendy Songs</p>
+      <a href="https://open.spotify.com/playlist/3tzVgXTTkeQO7YsfREXUed?si=56b2bda593d54f98" class="hover:brightness-75 hover:scale-105 transition-all duration-300" target="_blank" rel="noopener noreferrer">
+        <img src="../../icons/spotify.svg" alt="logoSpotify" width="20">
+      </a>
+    </div>
+    <div v-for="(item, index) in songs " :key="index" class="flex gap-x-2 items-center justify-between border-b border-b-slate-700 last:border-b-0 py-2">
+      <div class="flex items-center gap-x-4">
+        <picture class="rounded-lg overflow-clip">
+          <img width="60" :src=item.track.album.images[2].url alt="">
+        </picture>
+        <p class="font-bold text-white">{{ item.track.name }}</p>
+      </div>
+        <div class="bg-slate-900 size-12 rounded-full flex items-center justify-center hover:bg-slate-700 transition-colors duration-300 cursor-pointer" @click="playSong(item,index)">
+          <audio :id="`spotiSong_${index}`" :src=item.track.preview_url></audio>
+          <play-icon :is-play="checkProp(index)"></play-icon>
+        </div>
+      </div>
+    </div>
+</template>
